f0569be6f6fadfaf3e82e48d41d9b5af6a11ff0c
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-09-05 20:59:12 -0400

    Updated project card animation

--- scripts.js
+++ scripts.js
@@ -3,17 +3,27 @@ const footer = document.querySelector("footer");
 footer.textContent = `© ${new Date().getFullYear()} Mohamed Ibrahim`
 
 const aboutMe = document.querySelector(".about-me");
-const projects = document.querySelector(".projects");
+const projectSection = document.querySelector(".projects");
 const contact = document.querySelector(".contact");
+const allProjects = document.querySelectorAll(".project");
 
-const pageSections = [aboutMe, projects, contact];
+const pageSections = [aboutMe, projectSection, contact];
 
 const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        entry.target.classList.toggle("show", entry.isIntersecting);
-    });
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.toggle("show");
+      observer.unobserve(entry.target)
+      }
+  })
+}, 
+{
+  threshold: 0.4
 })
 
 pageSections.forEach(section => {
-    observer.observe(section);
+  observer.observe(section);
+})
+allProjects.forEach(project => {
+  observer.observe(project);
 })
